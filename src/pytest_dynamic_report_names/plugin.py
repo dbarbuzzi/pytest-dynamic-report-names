6ca766b0ffe2daf5d70b21563c0d687be73e5023
Domenic Barbuzzi <dbarbuzzi@gmail.com>
2025-01-03 13:22:48 -0500

    Refactor to separate functions

--- src/pytest_dynamic_report_names/plugin.py
+++ src/pytest_dynamic_report_names/plugin.py
@@ -7,25 +7,32 @@ def get_utc_timestamp() -> float:
     return datetime.now(timezone.utc).timestamp()
 
 
+def generate_junit_flags() -> list[str]:
+    if not (junitxml_base_dir := os.getenv("PDRN_JUNIT_BASE")):
+        return []
+
+    junitxml_file = Path(junitxml_base_dir) / f"{get_utc_timestamp()}.xml"
+
+    if prefix := os.getenv("PDRN_JUNIT_PREFIX"):
+        junitxml_file = junitxml_file.with_name(f"{prefix}{junitxml_file.name}")
+
+    return [f"--junit-xml={junitxml_file.as_posix()}"]
+
+
+def generate_coverage_flags() -> list[str]:
+    if not (cc_package_name := os.getenv("PDRN_COV_NAME")):
+        return []
+
+    return [
+        f"--cov={cc_package_name}",
+        "--cov-append",
+        "--cov-report=html:coverage-html",
+        "--cov-report=json:coverage.json",
+    ]
+
+
 def pytest_load_initial_conftests(_early_config, args: list[str], _parser):
     new_args: list[str] = []
-
-    # if PDRN_JUNIT_BASE is defined, prepare new --junit-xml flag
-    if junitxml_base_dir := os.getenv("PDRN_JUNIT_BASE"):
-        junitxml_file = Path(junitxml_base_dir) / f"{get_utc_timestamp()}.xml"
-        if prefix := os.getenv("PDRN_JUNIT_PREFIX"):
-            junitxml_file = junitxml_file.with_name(f"{prefix}{junitxml_file.name}")
-        new_args.append(f"--junit-xml={junitxml_file.as_posix()}")
-
-    # if PDRN_COV_NAME is defined, prepare code coverage flags
-    if cc_package_name := os.getenv("PDRN_COV_NAME"):
-        new_args.extend(
-            [
-                f"--cov={cc_package_name}",
-                "--cov-append",
-                "--cov-report=html:coverage-html",
-                "--cov-report=json:coverage.json",
-            ]
-        )
-
+    new_args.extend(generate_junit_flags())
+    new_args.extend(generate_coverage_flags())
     args[:] = [*args, *new_args]
